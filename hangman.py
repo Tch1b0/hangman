3588702d6410fab57c0c065ccea0d263d71ae303
Tch1b0 <Jpour3006@gmail.com>
2020-01-11 15:48:08 +0100

    asked if letter of guess is in word

--- hangman.py
+++ hangman.py
@@ -1,15 +1,20 @@
 import random
 Game = True
-
+charnum = 0
 while Game == True:
     Word = ["world","universe","ball","nintendo"]
     activeWord = (random.choice(Word))
 
     for char in activeWord:
        print ("/")
+       charnum = charnum + 1
+    print (charnum)
     guess = input()
     if guess.lower() in activeWord.lower():
-        print ("Right")
+        print ("Right letter")
+        quit()
+    elif guess.lower() is activeWord.lower():
+        print("Completely Right!")
         quit()
     else:
         print("Wrong")

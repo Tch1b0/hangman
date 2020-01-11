7379f9185982e8f4ccb4ab901c72fff3f83a8566
Tch1b0 <Jpour3006@gmail.com>
2020-01-11 19:02:33 +0100

    now you can't cheat anymore by typing in the same character again and again

--- hangman.py
+++ hangman.py
@@ -23,6 +23,7 @@ def endscreen(text,delay):              #the screen you see when you finish the 
 while Game == True:                     #While Game is true, the program gets executed
     gcharnum = 0                        #'gcharnum' gets reset/set to 0
     charnum = 0                         #'charnum' gets reset/set to 0
+    repeated = False                    #resetting repeated
 
     for char in activeWord :            #for every character of the searched word...
        print ("/")                      #... gets a character printed out as an slash
@@ -34,32 +35,34 @@ while Game == True:                     #While Game is true, the program gets ex
 
     if gcharnum == 1:                   #if just one character got guessed
 
-        if guess.lower() in activeWord: #right guess
-            print ("Right letter")
-            goodpoint = goodpoint + 1
-            string_value = guess.join(repeated_chars)
-
-        elif guess.lower() not in activeWord: #wrong guess
-            print ("That letter isn't in the Word")
-            badpoint = badpoint + 1     #1 badpoint is getting added
-            string_value = guess.join(repeated_chars)
-
-        elif guess in repeated_chars:
+        if guess in repeated_chars:     #if the character was already guessed
             print ("You already had this one")
-            badpoint = badpoint + 1
+            badpoint = badpoint + 1     #1 'badpoint' is getting added
+            repeated = True             #the other cases below get deactivated for the round
+
+        if guess.lower() in activeWord and repeated == False:      #right guess
+            print ("Right letter")
+            goodpoint = goodpoint + 1   #1 'goodpoint' is getting added
+            repeated_chars += [guess]   #the guess gets added to the 'repeated_chars' list and can't get guessed again
+
+        elif guess.lower() not in activeWord and repeated == False: #wrong guess
+            print ("That letter isn't in the Word")
+            badpoint = badpoint + 1     #1 'badpoint' is getting added
+            repeated_chars += [guess]   #the guess gets added to the 'repeated_chars' list and can't get guessed again
+
 
 
     elif gcharnum >= 1:                 #if the word got guessed 
         if guess.lower() == activeWord: #if the word was guessed right
             print("That's the word!")
-            quit() 
+            goodpoint = pointsneeded
         elif guess.lower() != activeWord: #if the word was guessed wrong 
             print("That's the wrong word.")
-            quit()             
+            badpoint = pointsneeded      
 
     
-    if badpoint == (pointsneeded):      #if you got as much mistakes like the word has characters, you lose
-        endscreen("lost",0.5)
+    if goodpoint == (pointsneeded):     #if you get every character of the word you win
+        endscreen("won",0.5)            #winning endscreen gets activated 
 
-    if goodpoint == (pointsneeded):     #if you get every character of the word (or write the same one again and again) you win
-        endscreen("won",0.5)
+    if badpoint == (pointsneeded):      #if you got as much mistakes like the word has characters, you lose
+        endscreen("lost",0.5)           #lose screen gets activated

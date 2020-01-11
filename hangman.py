4cb150ef8fd8bde6d45d29a865d9fd6b7105fbf7
Tch1b0 <Jpour3006@gmail.com>
2020-01-11 14:24:08 +0100

    initial commit

---
+++ hangman.py
@@ -0,0 +1,12 @@
+import random
+Word = ["World","Universe"]
+
+def Wordpick (): #Die Funktion nimmt sich ein Zufealliges Wort aus der Liste
+    activeWord = (random.choice(Word))
+    print (activeWord)
+
+
+Wordpick()
+lel = "hallo"
+genius = lel.count
+print (genius)

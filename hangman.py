7f947d1245bc6382b7a13dd82a4cb8bda0fcd42d
Tch1b0 <Jpour3006@gmail.com>
2020-01-11 15:56:43 +0100

    you can now guess the full word and letter

--- hangman.py
+++ hangman.py
@@ -1,22 +1,29 @@
 import random
 Game = True
 charnum = 0
+gcharnum = 0
 while Game == True:
-    Word = ["world","universe","ball","nintendo"]
+    Word = ["world","universe","ball",]
     activeWord = (random.choice(Word))
 
     for char in activeWord:
        print ("/")
        charnum = charnum + 1
-    print (charnum)
     guess = input()
-    if guess.lower() in activeWord.lower():
-        print ("Right letter")
-        quit()
-    elif guess.lower() is activeWord.lower():
-        print("Completely Right!")
-        quit()
-    else:
-        print("Wrong")
-        quit()
+    for char in guess:
+        gcharnum = gcharnum + 1
 
+    if gcharnum == 1:
+        if guess.lower() in activeWord:
+            print ("Right letter")
+            quit()
+        else:
+            print ("That letter isn't in the Word")
+            quit()
+    elif gcharnum >= 1:
+        if guess.lower() == activeWord:
+            print("That's the word!")
+            quit() 
+        elif guess.lower() != activeWord:
+            print("That's the wrong word.")
+            quit()

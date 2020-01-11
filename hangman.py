78f3a9249a011e113b6d5971f03007631ae6e04f
Tch1b0 <Jpour3006@gmail.com>
2020-01-11 15:01:17 +0100

    trying to figure out how to code this program

--- hangman.py
+++ hangman.py
@@ -5,12 +5,9 @@ if Game == True:
     Word = ["World","Universe"]
     activeWord = (random.choice(Word))
 
-    #for char in activeWord:
-    #    print(char, char.isalpha())
-
     for char in activeWord:
         print ("/")
 
     guess = input()
-    for cahr in activeWord:
+    for char in activeWord:
         print(char, char.isalpha)
